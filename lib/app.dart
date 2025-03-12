c85b8052c3cce4f7fd821f421ae1e595547c5c50
Mikhail Zharov <mike.zharov91@gmail.com>
2025-03-12 03:50:36 +0300

    Changed router location

--- lib/app.dart
+++ lib/app.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
-import 'package:rick_demo_project/presentation/router/routes_list.dart';
+import 'package:rick_demo_project/presentation/ui/router/routes_list.dart';
 
 class Application extends StatelessWidget{
   Application({super.key});
--- lib/presentation/ui/router/routes_list.dart
+++ lib/presentation/ui/router/routes_list.dart
@@ -1,12 +1,12 @@
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:go_router/go_router.dart';
 import 'package:rick_demo_project/core/injection/service_locator.dart';
+import 'package:rick_demo_project/presentation/blocs/characters/characters_bloc.dart';
+import 'package:rick_demo_project/presentation/blocs/favorite_characters/favorite_characters_bloc.dart';
 import 'package:rick_demo_project/presentation/ui/pages/characters_page.dart';
 import 'package:rick_demo_project/presentation/ui/pages/favorites_page.dart';
+import 'package:rick_demo_project/presentation/ui/widgets/common/custom_navigation_bar.dart';
 
-import '../blocs/characters/characters_bloc.dart';
-import '../blocs/favorite_characters/favorite_characters_bloc.dart';
-import '../ui/widgets/common/custom_navigation_bar.dart';
 
 final List<RouteBase> routesList = [
   StatefulShellRoute.indexedStack(
@@ -17,7 +17,7 @@ final List<RouteBase> routesList = [
           GoRoute(
             path: '/characters',
             builder: (context, state) => BlocProvider(
-              create: (context) => getIt<CharactersBloc>(),
+              create: (context) => getIt<CharactersBloc>()..add(FetchCharactersEvent()),
               child: const CharactersPage(),
             ),
           )
@@ -28,7 +28,7 @@ final List<RouteBase> routesList = [
             GoRoute(
                 path: '/favorites',
               builder: (context, state) => BlocProvider(
-                create: (context) => getIt<FavoriteCharactersBloc>(),
+                create: (context) => getIt<FavoriteCharactersBloc>()..add(FetchFavoriteCharactersEvent()),
                 child: const FavoritesPage(),
               ),
             )
